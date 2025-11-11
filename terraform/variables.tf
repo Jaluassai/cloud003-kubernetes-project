a1728bc5dabc33444448d4d65fe8640584781a84
Jaluassai <guzdekpl@gmail.com>
2025-11-11 14:45:28 +0100

    variables configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,29 @@
 variable proxmox_api_url {
+    description = "api url"
     type = string
+    sensitive = false
 }
 
 variable proxmox_api_token_id {
+    description = "api token id"
     type = string
+    sensitive = true
 }
 
 variable proxmox_api_token_secret {
+    description = "api token secret"
     type = string
+    sensitive = true
 }
 
 variable PROXMOX_VE_USERNAME {
+    description = "username for terraform account"
     type = string
+    sensitive = true
 }
 
 variable PROXMOX_VE_PASSWORD {
+    description = "passwd for terraform account"
     type = string
+    sensitive = true
 }
