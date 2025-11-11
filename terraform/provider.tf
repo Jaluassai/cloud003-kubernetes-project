83951a170121a9f723f311bed68597a8f9eb1869
Jaluassai <guzdekpl@gmail.com>
2025-11-11 14:39:25 +0100

    moved variables to a separated file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,26 +7,6 @@ terraform {
   }
 }
 
-variable proxmox_api_url {
-    type = string
-}
-
-variable proxmox_api_token_id {
-    type = string
-}
-
-variable proxmox_api_token_secret {
-    type = string
-}
-
-variable PROXMOX_VE_USERNAME {
-    type = string
-}
-
-variable PROXMOX_VE_PASSWORD {
-    type = string
-}
-
 provider "proxmox" {
   endpoint = var.proxmox_api_url
   api_token = var.proxmox_api_token_id
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable proxmox_api_url {
+    type = string
+}
+
+variable proxmox_api_token_id {
+    type = string
+}
+
+variable proxmox_api_token_secret {
+    type = string
+}
+
+variable PROXMOX_VE_USERNAME {
+    type = string
+}
+
+variable PROXMOX_VE_PASSWORD {
+    type = string
+}
