4e1b4d883164f43c1f4744044047f9809c3c6ded
Jaluassai <guzdekpl@gmail.com>
2025-11-12 21:09:04 +0100

    initial configuration of main terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name      = "test-ubuntu"
+  node_name = "Hades01"
+
+  stop_on_destroy = true
+
+  initialization {
+    user_account {
+    }
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
+
+  cpu {
+    type = "host"
+    cores = 2
+    numa = true
+  }
+
+  memory {
+    dedicated = 1024
+  }
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name = "Hades01"
+  overwrite_unmanaged = true
+  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  file_name = "noble-server-cloudimg-amd64.img.qcow2"
+}
