c27df83f6ca9c92a7a89bbb0e1045d902ba316dd
Jaluassai <guzdekpl@gmail.com>
2025-11-19 20:36:18 +0100

    added worker nodes for demeter

--- terraform/environments/demeter/main.tf
+++ terraform/environments/demeter/main.tf
@@ -1,7 +1,7 @@
 module "cloud_image" {
   source    = "../../modules/cloud_image"
 }
-
+# Master nodes
 module "vms" {
   source = "../../modules/vm"
 
@@ -23,3 +23,24 @@ module "vms" {
   secret_password = var.secret_password
 }
 
+# Worker nodes
+module "worker_vms" {
+  source = "../../modules/vm"
+
+  environment     = "demeter"
+  vm_count        = 3
+  name_prefix     = "worker"
+  node_name       = "Hades01"
+  starting_vm_id  = 3050
+
+  cloud_image_id  = module.cloud_image.image_id
+  disk_size       = 20
+  cpu_cores       = 2
+  memory          = 2048
+  network_bridge  = "vmbr0"
+  ip_addresses    = ["192.168.100.135/24", "192.168.100.136/24", "192.168.100.137/24"]
+  gw              = "192.168.100.1"
+
+  basic_account   = var.basic_account
+  secret_password = var.secret_password
+}
