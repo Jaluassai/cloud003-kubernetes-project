e97cd2fa446b1425f05b4fff572a28400eaf91f5
Jaluassai <guzdekpl@gmail.com>
2025-11-19 20:35:21 +0100

    configured demeter for deploying master nodes

--- terraform/environments/demeter/main.tf
+++ terraform/environments/demeter/main.tf
@@ -1,6 +1,5 @@
 module "cloud_image" {
   source    = "../../modules/cloud_image"
-  node_name = "Hades01"
 }
 
 module "vms" {
@@ -8,16 +7,19 @@ module "vms" {
 
   environment     = "demeter"
   vm_count        = 3
-  name_prefix     = "ubuntu"
+  name_prefix     = "master"
   node_name       = "Hades01"
   starting_vm_id  = 3000
 
   cloud_image_id  = module.cloud_image.image_id
-  disk_size       = 40
+  disk_size       = 20
   cpu_cores       = 4
   memory          = 4096
   network_bridge  = "vmbr0"
+  ip_addresses    = ["192.168.100.131/24", "192.168.100.132/24", "192.168.100.133/24"]
+  gw              = "192.168.100.1"
 
   basic_account   = var.basic_account
   secret_password = var.secret_password
 }
+
